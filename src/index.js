6a4f726fa18876eccb83bbb1cf8fb0dcdfa7e18a
Josh van Eyken <josh.vaneyken@gmail.com>
2023-04-03 21:46:50 -0700

    finish event listeners for sidebar buttons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 // https://api.nytimes.com/svc/books/v3/reviews.json
 
-const apiURL = "https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=73W0ByfNVTxMkbhcn7rMWYUVQGPDej9z";
+// const { create } = require("core-js/core/object");
+// const { async } = require("regenerator-runtime");
+
+const apiURL = "https://api.nytimes.com/svc/books/v3/lists.json?api-key=73W0ByfNVTxMkbhcn7rMWYUVQGPDej9z";
 
 let list = [];
 
+let listName = "";
+
 let searchInput = document.querySelector("#search-input");
 let mainContainer = document.querySelector("main");
+let listUrl = "";
 
 async function fetchData() {
-    console.log(searchInput.value);
-    let response = await fetch(apiURL + `&bestsellers-date=${searchInput.value}-01-01`);
+    let response = await fetch(listUrl);
     if (response.ok) {
         let body = await response.json();
         list = body.results;
@@ -21,18 +26,182 @@ async function fetchData() {
 
 function createList() {
     list.forEach(result => {
-        let listItem = document.createElement("li");
-        listItem.innerHTML = result.book_details[0].title;
-        mainContainer.appendChild(listItem);
+        let listDiv = document.createElement("div");
+        let listTitle = document.createElement("h1");
+        let listAuthor = document.createElement("p");
+        let listDescription = document.createElement("p");
+        listTitle.innerHTML = result.book_details[0].title;
+        listAuthor.innerHTML = result.book_details[0].author;
+        listDescription.innerHTML = result.book_details[0].description;
+        listDiv.appendChild(listTitle);
+        listDiv.appendChild(listAuthor);
+        listDiv.appendChild(listDescription);
+        mainContainer.appendChild(listDiv);
     })
 }
 
-let searchButton = document.getElementById("search-button");
-searchButton.addEventListener('click', async e => {
-    e.preventDefault();
-    await fetchData();
-    createList();
+
+
+
+let hardcoverFictionButton = document.getElementById("hardcover-fiction-button")
+hardcoverFictionButton.addEventListener('click', async e => {
+    listName = "list=hardcover-fiction";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+    
 })
+
+let paperbackFictionButton = document.getElementById("paperback-fiction-button")
+paperbackFictionButton.addEventListener('click', async e => {
+    listName = "list=trade-fiction-paperback";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let hardcoverNonfictionButton = document.getElementById("hardcover-nonfiction-button")
+hardcoverNonfictionButton.addEventListener('click', async e => {
+    listName = "list=hardcover-nonfiction";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let paperbackNonfictionButton = document.getElementById("paperback-nonfiction-button")
+paperbackNonfictionButton.addEventListener('click', async e => {
+    listName = "list=paperback-nonfiction";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let adviceButton = document.getElementById("advice-button")
+adviceButton.addEventListener('click', async e => {
+    listName = "list=advice-how-to-and-miscellaneous";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let midGradeHardcoverButton = document.getElementById("mid-grade-hardcover-button")
+midGradeHardcoverButton.addEventListener('click', async e => {
+    listName = "list=childrens-middle-grade-hardcover";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let pictureBookButton = document.getElementById("picture-book-button")
+pictureBookButton.addEventListener('click', async e => {
+    listName = "list=picture-books";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let childrenSeries = document.getElementById("children-series-button")
+childrenSeries.addEventListener('click', async e => {
+    listName = "list=series-books";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+let youngAdultHardcover = document.getElementById("ya-hardcover-button")
+youngAdultHardcover.addEventListener('click', async e => {
+    listName = "list=young-adult-hardcover";
+    listUrl = `${apiURL}&${listName}`
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.firstChild);
+    }
+        e.preventDefault();
+        await fetchData();
+
+        let ListHeader = document.createElement("h1");
+        ListHeader.innerHTML = list[0].display_name;
+        mainContainer.appendChild(ListHeader);
+        createList();
+})
+
+
+
+
+
+// let searchButton = document.getElementById("search-button");
+// searchButton.addEventListener('click', async e => {
+//     e.preventDefault();
+//     await fetchData();
+//     createList();
+// })
+
+
+
+
 
 // window.fetchData = fetchData;
 // window.createList = createList;
